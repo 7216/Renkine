865c7b0b629478a7c6cdbdbb24c31c0bdac2c9a0
Mikko Tikkanen <destwa@gmail.com>
2015-12-13 17:33:08 +0200

    Removed Math.h combined now with renkine.h for simplicity.

--- Renkine/math.h
+++ Renkine/math.h
@@ -1,10 +1,5 @@
 #ifndef RENKINE_MATH_H
 #define RENKINE_MATH_H
 
-#include "Math\math.h"
-#include "Math\Vector2.h"
-#include "Math\Vector3.h"
-#include "Math\Vector4.h"
-#include "Math\Matrix4.h"
 
 #endif // !RENKINE_MATH_MATH_H
