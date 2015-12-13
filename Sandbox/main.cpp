e1795e102bfe792204506c8f575c6cbe1e1d98b6
Jyri Puputti <jyri.puputti@gmail.com>
2015-12-13 12:11:57 +0200

    Cleaning

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -9,29 +9,27 @@
 
 int main()
 {
-	GLFWwindow* window;
-
-	/* Initialize the library */
 	if (!glfwInit())
+	{
 		return -1;
+	}
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(1280, 720, "Renkine", NULL, NULL);
-
-	int width = 0, height = 0;
-	glfwGetMonitorPhysicalSize (glfwGetPrimaryMonitor (), &width, &height);
-	glfwSetWindowPos (window, width / 2, height / 2);
-
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Renkine", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
 
+	glfwMakeContextCurrent(window);
 	glewInit ();
+
+	int width = 0, height = 0;
+	glfwGetMonitorPhysicalSize (glfwGetPrimaryMonitor (), &width, &height);
+	glfwSetWindowPos (window, width / 2, height / 2);
+
+	
 
 	float left = 0.0f;
 	float right = 1280.0f;
@@ -71,25 +69,16 @@ int main()
 
 	renkine::Shader shader = renkine::Shader ("test.vert", "test.frag");
 
-	/*renkine::Vector2 a = renkine::Vector2(5.0f, 5.0f);
-	renkine::Vector2 b = renkine::Vector2(5.0f, 5.0f);
-	renkine::Vector2 c = a*b;
-	printf("%f, %f", c.x, c.y);
-	*/
-	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
 		renkine::Graphics::Clear (renkine::RGB (255, 0, 255));
 
 		shader.SetUniformMatrix4 ("MVP_Matrix", MVP_Matrix);
 		shader.Enable ();
 		renkine::Renderer2D::Render (renderable, {0.f, 0.f}, 0);
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
