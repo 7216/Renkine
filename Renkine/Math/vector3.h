0a83bc1643ca1e5a77e08f1447edab69e8073845
Jyri Puputti <jyri.puputti@gmail.com>
2015-12-13 11:25:16 +0200

    ...

--- Renkine/Math/vector3.h
+++ Renkine/Math/vector3.h
@@ -40,20 +40,20 @@ namespace renkine
 		void Rotate (const float angle);
 
 		bool Equals (const Vector3 &other );
-		bool Equals (const float &other);
+		bool Equals (const float other);
 
-		Vector3 operator + (const float &other);
-		Vector3 operator - (const float &other);
-		Vector3 operator * (const float &other);
-		Vector3 operator / (const float &other);
+		Vector3 operator + (const float other);
+		Vector3 operator - (const float other);
+		Vector3 operator * (const float other);
+		Vector3 operator / (const float other);
 
-		bool operator == (const float &other);
-		bool operator != (const float &other);
+		bool operator == (const float other);
+		bool operator != (const float other);
 
-		void operator += (const float &other);
-		void operator -= (const float &other);
-		void operator *= (const float &other);
-		void operator /= (const float &other);
+		void operator += (const float other);
+		void operator -= (const float other);
+		void operator *= (const float other);
+		void operator /= (const float other);
 
 		Vector3 operator + (const Vector3 &other);
 		Vector3 operator - (const Vector3 &other);
